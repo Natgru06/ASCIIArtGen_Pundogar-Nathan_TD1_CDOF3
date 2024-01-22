81770774ae2a6bd9ae833ee15f8b2264cb172965
Flozoui <96843353+Flozoui@users.noreply.github.com>
2024-01-22 19:19:02 +0100

    Update projet.py
    add the list of fonts

--- projet.py
+++ projet.py
@@ -5,11 +5,18 @@ def generate_ascii_art(text, font='standard'):
     ascii_art = fig.renderText(text)
     return ascii_art
 
+def print_available_fonts():
+    print("Available Fonts:")
+    for font in Figlet().getFonts():
+        print(font)
+
 if __name__ == "__main__":
     input_text = input("Enter text to convert into ASCII art: ")
+
+    print_available_fonts()
     font_choice = input("Enter font (or press Enter for standard): ")
 
-    if not font_choice:
+    if not font_choice or font_choice not in Figlet().getFonts():
         font_choice = 'standard'
 
     result = generate_ascii_art(input_text, font_choice)
